6cab0907281254a09b6f5ddcdcaee92bb90a9173
AnkushChharri <viramachhari@gmail.com>
2024-07-20 14:17:28 +0530

    Try

--- src/app/Emoji/arrow-emoji/ArrowEmojis.js
+++ src/app/Emoji/arrow-emoji/ArrowEmojis.js
@@ -1,11 +1,10 @@
 "use client"
 
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef, useMemo, useEffect } from 'react';
 import { Copy, X } from 'lucide-react';
 import Link from 'next/link';
 
 const symbols = [
-    // Arrow emojis
     '➡️', '⬅️', '⬆️', '⬇️', '↗️', '↘️', '↙️', '↖️', '↕️', '↔️', '↩️', '↪️',
     '⤴️', '⤵️', '🔄', '🔃', '🔂', '🔁', '🔀', '🔼', '🔽', '⏫', '⏬', '⏩', '⏪',
     '🔚', '🔙', '🔛', '🔝', '🔜', '☑️', '✅', '✔️', '❇️', '✳️', '➿', '➰', '〽️',
@@ -16,6 +15,24 @@ const symbols = [
 const SymbolSelector = () => {
     const [selectedSymbols, setSelectedSymbols] = useState('');
     const [copiedSymbol, setCopiedSymbol] = useState(null);
+    const textareaRef = useRef(null);
+
+    const memoizedSymbols = useMemo(() => symbols, []);
+
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+            if (event.ctrlKey && event.key === 'v') {
+                navigator.clipboard.readText().then(
+                    clipText => setSelectedSymbols(prev => prev + clipText)
+                );
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyPress);
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, []);
 
     const handleSymbolClick = useCallback((symbol) => {
         setSelectedSymbols(prev => prev + symbol);
@@ -38,6 +55,9 @@ const SymbolSelector = () => {
 
     const handleClear = useCallback(() => {
         setSelectedSymbols('');
+        if (textareaRef.current) {
+            textareaRef.current.focus();
+        }
     }, []);
 
     return (
@@ -45,6 +65,7 @@ const SymbolSelector = () => {
             <div className="mb-4">
                 <div className="relative p-4 rounded-md">
                     <textarea
+                        ref={textareaRef}
                         value={selectedSymbols}
                         onChange={(e) => setSelectedSymbols(e.target.value)}
                         className="rounded-md p-4 w-full h-24 focus:ring-1 outline-none focus:ring-sky-500 border focus:border-sky-300 ring-zinc-400/75 shadow-sm hover:ring-sky-300 bg-zinc-50 shadow-zinc-600"
@@ -67,72 +88,51 @@ const SymbolSelector = () => {
             </div>
 
             <div className="text-center pb-1 overflow-x-auto" style={{ width: '100%', whiteSpace: 'nowrap' }}>
-
                 <Link href="/" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                     Fancy Font
                 </Link>
-
                 <Link href="/discord-emoji" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                     Discord Emoji
                 </Link>
-
-
-
                 <Link href="/bullet-point-symbol" className="inline-block text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                     Bullet Point Symbol
                 </Link>
-
                 <Link href="/zalgo-text" className="inline-block text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                     Zalgo Text
                 </Link>
-
             </div>
 
             <div className="text-center ring-cyan-300 pb-4 pt-3 overflow-x-auto" style={{ width: '100%', whiteSpace: 'nowrap' }}>
-
                 <Link href="/emoji/moon-emoji" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-3 py-2 text-xs text-center me-2 mb-2">
                     Moon Emoji
                 </Link>
-
                 <Link href="/emoji/fire-emoji" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-xs px-3 py-2 text-center me-2 mb-2">
                     Fire Emoji
                 </Link>
-
                 <Link href="/emoji/heart-emoji" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xs px-3 py-2 text-center me-2 mb-2">
                     Heart Emoji
                 </Link>
-
                 <Link href="/emoji/home-emoji" className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-xs px-3 py-2 text-center me-2 mb-2">
                     Home Emoji
                 </Link>
-
                 <Link href="/emoji/Kaomoji" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-xs px-3 py-2 text-center me-2 mb-2">
                     Kaomoji
                 </Link>
-
                 <Link href="/emoji/mewing-emoji" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-xs px-3 py-2 text-center me-2 mb-2">
                     Mewing Emoji
                 </Link>
-
                 <Link href="/emoji/skull-emoji" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-3 py-2 text-xs text-center me-2 mb-2">
                     Skull Emoji
                 </Link>
-
                 <Link href="/emoji/dot-emoji" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-xs px-3 py-2 text-center me-2 mb-2">
                     Dot Emoji
                 </Link>
-
-
-
-
             </div>
-
-
 
             <div className="mb-6">
                 <h3 className="text-lg font-semibold mb-2 text-gray-700">Comprehensive Symbols</h3>
                 <div className="grid grid-cols-8 sm:grid-cols-10 md:grid-cols-12 lg:grid-cols-15 gap-2">
-                    {symbols.map((symbol, index) => (
+                    {memoizedSymbols.map((symbol, index) => (
                         <div
                             key={index}
                             className="relative flex items-center justify-center bg-white p-2 rounded cursor-pointer hover:bg-gray-200 transition-colors duration-200"
